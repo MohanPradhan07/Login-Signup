e507b8268738d04374008fe5756e1dd146bd8821
Mohan Pradhan <pradhanmohan058@gmail.com>
2026-01-03 14:29:05 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -29,7 +29,7 @@ function Home() {
         return;
       }
 
-      const response = await fetch("http://localhost:8080/products", {
+      const response = await fetch("login-signup-two-pink.vercel.app/products", {
         method: "GET",
         headers: {
           Authorization: `Bearer ${token}`,
