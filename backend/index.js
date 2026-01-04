99756fc113065b8c97f28f22822dc29b5934b180
Mohan Pradhan <pradhanmohan058@gmail.com>
2026-01-04 16:32:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "*" }));
 
 app.use("/auth", AuthRouter);
 app.use("/products", ProductRouter);
